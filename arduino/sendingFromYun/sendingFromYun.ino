358868ba1b7713a6217dd5a948448ce8e6c0094e
cristianReynaga <cristianreynaga@gmail.com>
2014-08-12 13:30:57 -0300

    update parsing

--- arduino/sendingFromYun/sendingFromYun.ino
+++ arduino/sendingFromYun/sendingFromYun.ino
@@ -1,31 +1,3 @@
-/*
-  Console Pixel
-
- An example of using the Arduino board to receive data from the
- Console on the Arduino Yún.  In this case, the Arduino boards turns on an LED when
- it receives the character 'H', and turns off the LED when it
- receives the character 'L'.
-
- To see the Console, pick your Yún's name and IP address in the Port menu
- then open the Port Monitor. You can also see it by opening a terminal window
- and typing
- ssh root@ yourYunsName.local 'telnet localhost 6571'
- then pressing enter. When prompted for the password, enter it.
-
-
- The circuit:
- * LED connected from digital pin 13 to ground
-
- created 2006
- by David A. Mellis
- modified 25 Jun 2013
- by Tom Igoe
-
- This example code is in the public domain.
-
- http://arduino.cc/en/Tutorial/ConsolePixel
-
- */
 
 #include <Console.h>
 
@@ -41,8 +13,6 @@ void setup() {
 
   // Wait for the Console port to connect
   while (!Console);
-
-  //  Console.println("type H or L to turn pin 13 on or off");
 
   // initialize the LED pin as an output:
   pinMode(ledPin, OUTPUT);
@@ -63,16 +33,16 @@ void loop() {
     digitalWrite(ledPin, LOW);
   }
   int sensor = analogRead(0);
-    Console.print(header);
-    delay(del);
-    Console.print(100);
+   // Console.print(header);
+    //delay(del);
+    Console.print(sensor);
     delay(del);
     //Console.print(",");
     //Console.print(404);
     //Console.print(",");
     //Console.print(504);
-    Console.print(tail);
-    delay(del);
+//    Console.print(tail);
+  //  delay(del);
     //  Console.println();
     // Console.print('K');
     //Console.print(604);
